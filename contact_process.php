bd04fb80b791177e1772ca86c26da9eb4cb4460d
Akshay <akshaikumarsk@gamil.com>
2022-07-07 22:27:32 +0530

    email ckecking no photo and about

---
+++ contact_process.php
@@ -0,0 +1,26 @@
+<?php
+
+    $to = "akshaikumarsk@gmail.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $subjectt = $_REQUEST['subject'];
+    $number = $_REQUEST['number'];
+    $cmessage = $_REQUEST['message'];
+
+    $headers = "From: $from";
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    $subject = "You have a message from your Persional website.".$sujectt;
+
+    $txt ="Name =".$name."\r\n Email = ". $from."\r\n Subject =" .$subject."\r\n Message = ". $cmessage;
+
+	  
+
+    $send = mail($to, $subject, $txt, $headers);
+
+	echo '<script> alert("Mail send")</script>';
+
+?>
